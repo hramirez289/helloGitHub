6aa43ce9b9ea188f42e0d18a61f032f2cfec112f
hramirez289 <hernan.stgohc@gmail.com>
2023-11-12 00:58:09 -0300

    segundo commit

---
+++ helloGitHub/src/main/java/com/mycompany/hellogithub/dlaiwdj.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hellogithub;
+
+/**
+ *
+ * @author Hernan
+ */
+public class dlaiwdj {
+    public void saludar(){
+        System.out.println("hola mud");
+
+    }
+    
+    
+}
